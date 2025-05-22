0be1526550fc5fd89e9a8fb647108cb996abee94
SuvratZZZ <singhsubrat35@gmail.com>
2025-05-22 20:17:50 +0530

    added svgs

--- client/src/utils/vars.ts
+++ client/src/utils/vars.ts
@@ -1,6 +1,6 @@
 // Backend API configuration
 export let BACKEND_URL = 'https://quiz-battle-royale-production.up.railway.app';
-// BACKEND_URL = 'http://localhost:5000';
+BACKEND_URL = 'http://localhost:5000';
 export const API_URL = `${BACKEND_URL}/api`;
 export const SOCKET_URL = BACKEND_URL;
 
